63290ad284f3fed72c29a0f02ac39a3b379e1b44
Daniel Martins <danielmcoelho@ua.pt>
2019-10-01 17:39:08 +0100

    Account Controller was created and updated; Auxiliar Functions was created and updated; Models was created and updated;

--- Project/account_controller.py
+++ Project/account_controller.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify, request, Blueprint
-from auxiliar_functions import *
+from auxiliar_functions import Auxiliar, Message
+from http import HTTPStatus
 import datetime
 import uuid
+
+# CONFIG
+account_controller = Blueprint('account', __name__,)
 
 @account_controller.route('/account', methods=['POST'])
 def create_account():
@@ -10,15 +14,37 @@ def create_account():
 
         :rtype: dict | bytes
     """
+    
+    code = HTTPStatus.OK
+    aux = Auxiliar()
+    msg = Message()
+    
     try:
+        # Get parameters
         user_id = request.json.get('user_id')
         currency = request.json.get('currency')
 
-        if currency not in currency_codes:
-            return status(ERROR)
+        # Validate the parameters
+        if aux.validate_uuid(user_id):
+            code = HTTPStatus.BAD_REQUEST
+            raise Exception("Your user_id is wrong. The user_id is not a universally unique identifier" )
+        elif currency not in aux.currency_codes():
+            code = HTTPStatus.BAD_REQUEST
+            raise Exception("Your currency is wrong. Uses the international standard that defines three-letter codes as currencies established by the International Organization. (ISO 4217)" )
+
+    except Exception as excep :
+        return msg.message(code,str(excep))
+
+    # Save the account with the user id
+    
+
+    # Return the information
+    date = datetime.datetime.utcnow().isoformat()
+    return msg.message(code, jsonify(id=uuid.uuid4(),user_id=user_id,balance=0,currency=currency,state="activate", created_at=date, updated_at=date))
 
 
-@user_controller.route('/account/<int:id>/amount', methods=['POST'])
+
+@account_controller.route('/account/<int:id>/amount', methods=['POST'])
 def add_amount(id):
     """
         Add an amount to an account
@@ -29,7 +55,7 @@ def add_amount(id):
 	    :rtype: dict | bytes
     """
 
-@user_controller.route('/account/<int:id>/activate', methods=['POST'])
+@account_controller.route('/account/<int:id>/activate', methods=['POST'])
 def activate_account(id):
     """
         Activate the user account
@@ -40,7 +66,7 @@ def activate_account(id):
 	    :rtype: dict | bytes    
     """
 
-@user_controller.route('/account/<int:id>/desactivate', methods=['POST'])
+@account_controller.route('/account/<int:id>/desactivate', methods=['POST'])
 def desativate_account(id):
     """
         Desactivate the user account
--- Project/auxiliar_functions.py
+++ Project/auxiliar_functions.py
@@ -1,12 +1,18 @@
-
 import urllib, re
+import uuid
+import datetime
+from flask import Flask, jsonify, request, Blueprint
 
 class Message:
-    def __init__(self):
+    def message(self, reason_code, message):
+        return jsonify(code = reason_code, message=message)
 
 
 class Auxiliar:
     
     def currency_codes(self):
         url = "http://www.iso.org/iso/support/faqs/faqs_widely_used_standards/widely_used_standards_other/currency_codes/currency_codes_list-1.htm"
-        return re.findall(r'\<td valign\="top"\>\s+([A-WYZ][A-Z]{2})\s+\</td\>', urllib.urlopen(url).read())
+        return re.findall(r'\<td valign\="top"\>\s+([A-WYZ][A-Z]{2})\s+\</td\>', urllib.request.urlopen(url).read())
+    
+    def validate_uuid(self,value):
+        return isinstance(value, uuid.UUID)
